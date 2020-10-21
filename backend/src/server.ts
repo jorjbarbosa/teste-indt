cc0d6bace73e606754645606dee06b3c1a8a95e6
Jorge Barbosa <jorgebarbosa_@outlook.com>
2020-10-20 21:21:16 -0400

    database config and products model

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,10 @@
 import express from 'express'
+import routes from './routes'
+import './database'
 
 const app = express();
 
-app.get('/', (request, response) => {
-    return response.json({msg: 'Servidor rodando'})
-})
+app.use(routes)
 
 app.listen(3333, () => {
     console.log('Servidor rodando na porta 3333')
