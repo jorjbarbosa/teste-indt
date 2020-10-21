1f3bce119fd9dbfab4c86636c55be12acea1d524
Jorge Barbosa <jorgebarbosa_@outlook.com>
2020-10-20 23:32:45 -0400

    Adicionando upload de imagem no cadastro de produtos

--- backend/src/routes/products.routes.ts
+++ backend/src/routes/products.routes.ts
@@ -1,12 +1,25 @@
 import { Router } from 'express'
 import StoreProductService from '../services/products/StoreProductService'
+import multer from 'multer'
+import uploadConfig from '../config/upload'
 
 const productsRouter = Router()
+const upload = multer(uploadConfig)
 
-productsRouter.post('/', async (request, response) => {
+productsRouter.post('/', upload.single('imagem'), async (request, response) => {
+    const {nome, descricao, valor, disponivel} = request.body
+    const imagem = request.file.filename
+
     try {
         const storeProduct = new StoreProductService()
-        const product = await storeProduct.store(request.body)
+        const product = await storeProduct.store({
+            nome,
+            descricao,
+            imagem,
+            valor,
+            disponivel
+        })
+        
         return response.json(product)
     } catch (err) {
         return response.status(400).json({error: err.message})
