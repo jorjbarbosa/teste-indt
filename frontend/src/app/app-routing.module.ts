e35984ba39ca03b015bf3ba389f4128cd429cfa2
Jorge Barbosa <jorgebarbosa_@outlook.com>
2020-10-22 21:42:28 -0400

    adicionando bootstrap e rotas

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { VitrineComponent } from './vitrine/vitrine.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'gerenciar-produtos',
+    component: VitrineComponent
+  },
+  {
+    path: '.',
+    component: VitrineComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,16 +6,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProdutoService } from './produto.service';
 import { VitrineComponent } from './vitrine/vitrine.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    VitrineComponent
+    VitrineComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    NgbModule
   ],
   providers: [ProdutoService, HttpClientModule],
   bootstrap: [AppComponent]
